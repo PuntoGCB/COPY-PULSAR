b902abbe7ca8d0f58e8b45430ae2bfea79792ad3
PuntoGCB <95536505+PuntoGCB@users.noreply.github.com>
2026-09-09 12:54:42 -0300

    Remove node-fetch import from server.js
    Removed the 'node-fetch' dependency from server.js.

--- server.js
+++ server.js
@@ -31,7 +31,6 @@
 
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Hostinger inyecta el puerto real
