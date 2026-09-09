d10fa7186722b1b801c3f1a0e62f63161eff5d28
PuntoGCB <95536505+PuntoGCB@users.noreply.github.com>
2026-09-09 12:57:01 -0300

    Add node-fetch dependency to server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@
 
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Hostinger inyecta el puerto real
